b63c2475586dcea7f7b0cf5ee9df03b66a7b6fcd
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-10-25 00:51:06 -0700

    Add example file

--- examples/basic.py
+++ examples/basic.py
@@ -1,16 +1,24 @@
-# Example script to create basic pipeline.
-#
-# Pipe(p1)
-# |
-# ---> ppr('parent')
-# |
-# ---> Pipe(p)
-#      |
-#      ---> ppr('s1')
-#      |
-#      ---> ppo('s', '2')
-#      |
-#      ---> ppr('p1'), ppo('p, '2')
+# -*- coding: utf-8 -*-
+
+"""
+Example script to create basic pipeline of the form.
+
+    Pipe(p)
+    |
+    ---> print_job
+    |
+    ---> Pipe(p1)
+         |
+         ---> print_job
+         |
+         ---> Pipe(p2)
+              |
+              ---> print_job
+              |
+              ---> print_job
+              |
+              ---> print_job, print_job
+"""
 
 import threading
 from time import sleep
@@ -18,40 +26,41 @@ from time import sleep
 from pypette import Job, Pipe
 
 
-def ppr(x):
+def print_job(message):
+    """Sample method which takes a message to print."""
     print(threading.currentThread().getName(), 'Starting')
     sleep(1)
-    print('From within ppr : ' + str(x))
+    print('From within print_job : ' + str(message))
     print(threading.currentThread().getName(), 'Ending')
 
 
-def ppo(a, b):
-    print(threading.currentThread().getName(), 'Starting')
-    sleep(1)
-    print('From within ppo : ' + str(a + b))
-    print(threading.currentThread().getName(), 'Ending')
-
-
-# Create pipeline p
-p = Pipe('p')
-p.add_jobs([
-    Job(ppr, ('s1',)),
-    Job(ppo, ('s', '2',))
+# Create pipeline p2
+p2 = Pipe('p2')
+p2.add_jobs([
+    Job(print_job, ('p2 1 series',)),
+    Job(print_job, ('p2 2 series',)),
 ])
-p.add_jobs([
-    Job(ppr, ('p1',)),
-    Job(ppo, ('p', '2',))
+p2.add_jobs([
+    Job(print_job, ('p2 3 parallel',)),
+    Job(print_job, ('p2 4 parallel',)),
 ], run_in_parallel=True)
 
 # Create pipeline p1
 p1 = Pipe('p1')
 p1.add_jobs([
-    Job(ppr, ('parent',)),
-    p
+    Job(print_job, ('p1 1 series',)),
+    p2
+])
+
+# Create pipeline p
+p = Pipe('p')
+p.add_jobs([
+    Job(print_job, ('top most',)),
+    p1
 ])
 
 # Display the structure of the pipeline to be run.
-p1.graph()
+p.graph()
 
 # Run the pipeline.
-p1.run()
+p.run()
