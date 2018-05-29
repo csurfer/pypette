bcadc8a635062bd7c5a8963080195d24530075ca
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2018-05-29 07:49:12 -0700

    Extend example to showcase all the newly added features

--- examples/basic.py
+++ examples/basic.py
@@ -15,7 +15,7 @@ Example script to create basic pipeline of the form.
   |          ⇩         ⇩
   |      print_job  Pipe(p2)
   |                 |
-  |                 ⇨  print_job
+  |                 ⇨  print_corrupt
   |                 |
   |                 ⇨  print_job
   |                 |
@@ -28,6 +28,10 @@ Example script to create basic pipeline of the form.
   ⇨  ls -l
   |
   ⇨  pwd
+  |
+  ⇨  Pipe(p3) <- Dependent on Pipe(p1)
+      |
+      ⇨  ls
 """
 
 __author__ = "Vishwas B Sharma"
@@ -49,9 +53,17 @@ def print_job(message):
     print(threading.currentThread().getName(), "Ending")
 
 
+def print_corrupt(message):
+    print(threading.currentThread().getName(), "Starting")
+    sleep(1)
+    print("From within print_corrupt : " + str(message))
+    print(threading.currentThread().getName(), "Ending")
+    raise Exception("lal")
+
+
 # Create pipeline p2
 p2 = Pipe("p2")
-p2.add_jobs([Job(print_job, ("p2 1",)), Job(print_job, ("p2 2",))])
+p2.add_jobs([Job(print_corrupt, ("p2 1",)), Job(print_job, ("p2 2",))])
 p2.add_jobs(
     [Job(print_job, ("p2 3.1",)), Job(print_job, ("p2 3.2",))],
     run_in_parallel=True,
@@ -64,9 +76,25 @@ p1.add_jobs([Job(print_job, ("p1 1",))])
 p1.add_jobs([Job(print_job, ("p1 2.1",)), p2], run_in_parallel=True)
 
 # Create pipeline p
+# When created as Pipe("p") or Pipe("p", Gate.FAIL_FAST) the pipeline exits the
+# execution in the first stage where it encounters exception.
+#
+# When created as Pipe("p", Gate.EXECUTE_ALL) the pipeline is forgiving of
+# exceptions and continues to execute all jobs even if an exception is thrwon
+# in a stage.
+#
+# Note that when an external dependency is added to the Pipeline, as showin in
+# line 93, the pipeline is not executed unless all of the pipelines are
+# mentioned as dependencies are successfully executed.
+
+# Create pipeline p3
+p3 = Pipe("p3")
+p3.add_jobs([BashJob(["ls"])])
+p3.add_dependency(p1, p2)
+
 p = Pipe("p")
 p.add_jobs(
-    [Job(print_job, ("p 1",)), p1, BashJob(["ls", "-l"]), BashJob(["pwd"])]
+    [Job(print_job, ("p 1",)), p1, BashJob(["ls", "-l"]), BashJob(["pwd"]), p3]
 )
 
 # Display the structure of the pipeline to be run.
@@ -74,3 +102,6 @@ p.graph()
 
 # Run the pipeline.
 p.run()
+
+# Report of pipeline.
+p.report()
