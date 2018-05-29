a56af41ed2cb32c23a5d83f676a0c2f9b5c33b38
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2018-05-29 07:39:44 -0700

    Extend pipes to add staging, reporting and dependency

--- pypette/pipes.py
+++ pypette/pipes.py
@@ -17,19 +17,37 @@ blocks.
 
 import logging
 from collections import OrderedDict
+from functools import partial
 from itertools import chain
 from uuid import uuid4
 
 import crayons
+from enum import Enum
 
 from .jobs import JobInterface
-from .threadwrapper import ThreadWrapper
+from .threadwrapper import ThreadState, ThreadWrapper
+
+
+class Gate(Enum):
+    """Different kinds of gating allowed for pipes."""
+
+    def __fail_fast():
+        return False
+
+    # Fails at the first error or exception thrown from a job. (Default)
+    FAIL_FAST = partial(__fail_fast)
+
+    def __execute_all():
+        return True
+
+    # Silently captures the error or exception to execute all the jobs.
+    EXECUTE_ALL = partial(__execute_all)
 
 
 class Pipe(object):
     """Class to define the pipeline structure."""
 
-    def __init__(self, name):
+    def __init__(self, name, gate=Gate.FAIL_FAST):
         """Constructor.
 
         :param name: Name given to the pipe object for identification.
@@ -37,6 +55,9 @@ class Pipe(object):
         """
         self.name = "Pipe({})".format(name)
         self.job_map = OrderedDict()
+        self.thread_map = OrderedDict()
+        self.gate = gate.value
+        self.dependent_on = []
 
     def add_jobs(self, jobs, run_in_parallel=False):
         """Method to add jobs to pipeline.
@@ -62,10 +83,65 @@ class Pipe(object):
         else:
             self._add_in_series(jobs)
 
+    def add_stage(self, *args):
+        """Method to add stages of pipeline. Jobs are to be given comma
+        separated and all the jobs given form a single stage. Another way to
+        add jobs to a pipeline in builder pattern.
+
+        :param args: Jobs to be added. Adds comma separated list in parallel or
+        a single job as a stage.
+        """
+
+        # Return if nothing to do.
+        if not args:
+            return
+
+        # Validate the set of jobs given.
+        Pipe._validate(args)
+
+        # Add jobs to pipeline.
+        if len(args) > 1:
+            self._add_in_parallel(list(args))
+        else:
+            self._add_in_series(args)
+
+        return self
+
+    def add_dependency(self, *args):
+        """Method to add dependency of one pipe on another.
+
+        :param args: Pipes to be added as dependency for this pipeline.
+        """
+        for job in args:
+            if not isinstance(job, Pipe):
+                logging.error("Dependecies should be of type Pipe")
+                raise AssertionError(
+                    "Invalid type {} submitted".format(type(job))
+                )
+        self.dependent_on.extend(args)
+
+    def dependency(self):
+        """Method to check for success of pipelines listed as dependency."""
+        allowed = True
+        for item in self.dependent_on:
+            allowed = allowed and (item.state == ThreadState.SUCCESS)
+        return allowed
+
     def run(self):
         """Method to run the pipeline."""
         logging.debug("run() method called on {}".format(self.name))
-        for jobset in self.job_map.values():
+
+        logging.debug(
+            "Dependency for {} is {}".format(self, self.dependent_on)
+        )
+        if not self.dependency():
+            return
+
+        broken = False
+        prev = True
+        for key, jobset in self.job_map.items():
+            if not prev:
+                break
             job_threads = []
             # Create job threads
             for job in jobset:
@@ -76,12 +152,81 @@ class Pipe(object):
             # Job main thread to create flow
             for job in job_threads:
                 job.join()
+            self.thread_map[key] = tuple(job_threads)
+            # Stage finished successfully.
+            for job in job_threads:
+                prev = prev and (job.state == ThreadState.SUCCESS)
+                if not prev:
+                    broken = True
+                prev = prev or self.gate()
+
+        if prev and not broken:
+            self.state = ThreadState.SUCCESS
+        else:
+            self.state = ThreadState.FAILED
 
     def graph(self):
         """Method to print the structure of the pipeline."""
         logging.debug("graph() method called on {}".format(self.name))
         self._pretty_print()
         print("")
+
+    @staticmethod
+    def _cstate(tw):
+        """Returns state in colour for pretty printing reports."""
+        if isinstance(tw, ThreadWrapper):
+            if tw.state == ThreadState.SUCCESS:
+                return str(crayons.green(tw.state.name))
+            elif tw.state == ThreadState.FAILED:
+                return str(crayons.red(tw.state.name))
+            else:
+                return str(crayons.yellow(tw.state.name))
+        else:
+            if ThreadState.SUCCESS.name in tw:
+                return str(crayons.green(tw))
+            elif ThreadState.FAILED.name in tw:
+                return str(crayons.red(tw))
+            else:
+                return str(crayons.yellow(tw))
+
+    def report(self):
+        """Method to pretty print the report."""
+        print("")
+        print(crayons.green(self.name, bold=True))
+
+        if not self.thread_map:
+            print(crayons.red("No jobs run in pipeline yet !"))
+            return
+
+        joblen = len(self.thread_map)
+        for i, jobs in enumerate(self.thread_map.values()):
+            print(crayons.blue(u"| "))
+            if len(jobs) == 1:
+                print(crayons.blue(u"\u21E8  ") + Pipe._cstate(jobs[0]))
+            else:
+                if i == joblen - 1:
+                    pre = u"  "
+                else:
+                    pre = u"| "
+                l1 = [u"-" * 10 for j in jobs]
+                l1 = u"".join(l1)
+                l1 = l1[:-1]
+                print(crayons.blue(u"\u21E8 ") + crayons.blue(l1))
+                fmt = u"{0:^{wid}}"
+                l2 = [fmt.format(u"\u21E9", wid=12) for j in jobs]
+                print(crayons.blue(pre) + crayons.blue(u"".join(l2)))
+                l3 = [
+                    Pipe._cstate(fmt.format(j.state.name, wid=12))
+                    for j in jobs
+                ]
+                print(crayons.blue(pre) + u"".join(l3))
+
+        pipes = filter(
+            lambda x: isinstance(x.job, Pipe), chain(*self.thread_map.values())
+        )
+
+        for item in pipes:
+            item.job.report()
 
     @staticmethod
     def _validate(jobs):
--- tests/pipes_test.py
+++ tests/pipes_test.py
@@ -12,7 +12,7 @@ Usage from git root:
 import logging
 import unittest
 
-from pypette import BashJob, Job, Pipe
+from pypette import BashJob, Job, Pipe, ThreadState
 
 logging.basicConfig(level=logging.CRITICAL)
 
@@ -47,7 +47,7 @@ class PipeTest(unittest.TestCase):
         with self.assertRaises(AssertionError):
             Pipe._validate(["test_string"])
 
-    def test_basic_flow(self):
+    def test_add_jobs(self):
         """Validates the flow created due to submission of jobs to pipeline."""
 
         def dummy():
@@ -77,6 +77,85 @@ class PipeTest(unittest.TestCase):
             self.assertEqual(
                 a, b, "Jobset in the current stage of pipeline is different"
             )
+
+    def test_add_stages(self):
+        """Validates the flow created due to submission of jobs to pipeline."""
+
+        def dummy():
+            pass
+
+        j1 = Job(dummy)
+        b1 = BashJob(["ls"])
+        p = Pipe("test")
+
+        # Validate a new pipe contains no jobs by default.
+        self.assertEqual([], list(p.job_map.values()))
+
+        # Validate empty job list does nothing but doesn't throw an error
+        # either.
+        p.add_stage()
+        self.assertEqual([], list(p.job_map.values()))
+
+        # Validate the structure of the jobs submittted.
+        p.add_stage(j1)
+        p.add_stage(b1)
+        p.add_stage(b1)
+        p.add_stage(j1)
+        # Builder pattern
+        p.add_stage(j1, b1).add_stage(b1, j1)
+
+        expected = [[j1], [b1], [b1], [j1], [j1, b1], [b1, j1]]
+
+        for (a, b) in zip(p.job_map.values(), expected):
+            self.assertEqual(
+                a, b, "Jobset in the current stage of pipeline is different"
+            )
+
+    def test_add_dependency(self):
+        """Validates adding of dependencies to pipeline."""
+
+        p = Pipe("test")
+
+        p1 = Pipe("dependency1")
+        p.add_dependency(p1)
+        self.assertEqual([p1], p.dependent_on, "Dependency not as expected")
+
+        p2 = Pipe("dependency2")
+        p.add_dependency(p2)
+        self.assertEqual(
+            [p1, p2], p.dependent_on, "Dependency not as expected"
+        )
+
+        p3 = Pipe("dependency3")
+        p4 = Pipe("dependency4")
+        p.add_dependency(p3, p4)
+        self.assertEqual(
+            [p1, p2, p3, p4], p.dependent_on, "Dependency not as expected"
+        )
+
+    def test_dependency_check(self):
+        """Validates dependency check of pipeline."""
+
+        p = Pipe("test")
+
+        p1 = Pipe("dependency1")
+        p1.state = ThreadState.SUCCESS
+        p2 = Pipe("dependency2")
+        p2.state = ThreadState.SUCCESS
+        p3 = Pipe("dependency3")
+        p3.state = ThreadState.SUCCESS
+        p4 = Pipe("dependency4")
+        p4.state = ThreadState.SUCCESS
+
+        p.add_dependency(p1, p2, p3, p4)
+        self.assertTrue(
+            p.dependency(), "Dependency check expected to be successful"
+        )
+
+        p3.state = ThreadState.FAILED
+        self.assertFalse(
+            p.dependency(), "Dependency check expected to be a failure"
+        )
 
     def test_representative_name(self):
         """Validates the name for pipeline."""
@@ -137,6 +216,27 @@ class PipeTest(unittest.TestCase):
             p.run()
         except Exception:
             self.fail("run() should not throw any exception")
+
+    def test_report(self):
+        """Validates report call on pipelines."""
+        p = Pipe("test")
+
+        # Validate run call on empty pipe.
+        try:
+            p.report()
+        except Exception:
+            self.fail("report() on empty pipe should not throw any exception")
+
+        def dummy():
+            pass
+
+        p.add_jobs([Job(dummy), BashJob(["pwd"])])
+
+        try:
+            p.run()
+            p.report()
+        except Exception:
+            self.fail("report() should not throw any exception")
 
 
 if __name__ == "__main__":
