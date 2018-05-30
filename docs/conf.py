6fbfed7d4eea12b253582b29909e498c4ece77e2
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2018-05-30 08:10:18 -0700

    Version bump.

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Vishwas B Sharma'
 author = 'Vishwas B Sharma'
 
 # The short X.Y version
-version = '0.0.8'
+version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
