f322af2ef15d6358073c65098b1d7848ad850cbb
csurfer <sharma.vishwas88@gmail.com>
2021-09-10 14:30:02 -0700

    Add pre-commit checks to repo.

--- examples/basic.py
+++ examples/basic.py
@@ -34,10 +34,10 @@ Example script to create basic pipeline of the form.
       ⇨  ls
 """
 
-__author__ = "Vishwas B Sharma"
-__author_email__ = "sharma.vishwas88@gmail.com"
-__license__ = "MIT"
-__copyright__ = "Copyright 2017 Vishwas B Sharma"
+__author__ = 'Vishwas B Sharma'
+__author_email__ = 'sharma.vishwas88@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Vishwas B Sharma'
 
 import threading
 from time import sleep
@@ -47,33 +47,33 @@ from pypette import BashJob, Job, Pipe
 
 def print_job(message):
     """Sample method which takes a message to print."""
-    print(threading.currentThread().getName(), "Starting")
+    print(threading.currentThread().getName(), 'Starting')
     sleep(1)
-    print("From within print_job : " + str(message))
-    print(threading.currentThread().getName(), "Ending")
+    print('From within print_job : ' + str(message))
+    print(threading.currentThread().getName(), 'Ending')
 
 
 def print_corrupt(message):
-    print(threading.currentThread().getName(), "Starting")
+    print(threading.currentThread().getName(), 'Starting')
     sleep(1)
-    print("From within print_corrupt : " + str(message))
-    print(threading.currentThread().getName(), "Ending")
-    raise Exception("lal")
+    print('From within print_corrupt : ' + str(message))
+    print(threading.currentThread().getName(), 'Ending')
+    raise Exception('lal')
 
 
 # Create pipeline p2
-p2 = Pipe("p2")
-p2.add_jobs([Job(print_corrupt, ("p2 1",)), Job(print_job, ("p2 2",))])
+p2 = Pipe('p2')
+p2.add_jobs([Job(print_corrupt, ('p2 1',)), Job(print_job, ('p2 2',))])
 p2.add_jobs(
-    [Job(print_job, ("p2 3.1",)), Job(print_job, ("p2 3.2",))],
+    [Job(print_job, ('p2 3.1',)), Job(print_job, ('p2 3.2',))],
     run_in_parallel=True,
 )
-p2.add_jobs([Job(print_job, ("p2 4",))])
+p2.add_jobs([Job(print_job, ('p2 4',))])
 
 # Create pipeline p1
-p1 = Pipe("p1")
-p1.add_jobs([Job(print_job, ("p1 1",))])
-p1.add_jobs([Job(print_job, ("p1 2.1",)), p2], run_in_parallel=True)
+p1 = Pipe('p1')
+p1.add_jobs([Job(print_job, ('p1 1',))])
+p1.add_jobs([Job(print_job, ('p1 2.1',)), p2], run_in_parallel=True)
 
 # Create pipeline p
 # When created as Pipe("p") or Pipe("p", Gate.FAIL_FAST) the pipeline exits the
@@ -88,14 +88,12 @@ p1.add_jobs([Job(print_job, ("p1 2.1",)), p2], run_in_parallel=True)
 # mentioned as dependencies are successfully executed.
 
 # Create pipeline p3
-p3 = Pipe("p3")
-p3.add_jobs([BashJob(["ls"])])
+p3 = Pipe('p3')
+p3.add_jobs([BashJob(['ls'])])
 p3.add_dependency(p1, p2)
 
-p = Pipe("p")
-p.add_jobs(
-    [Job(print_job, ("p 1",)), p1, BashJob(["ls", "-l"]), BashJob(["pwd"]), p3]
-)
+p = Pipe('p')
+p.add_jobs([Job(print_job, ('p 1',)), p1, BashJob(['ls', '-l']), BashJob(['pwd']), p3])
 
 # Display the structure of the pipeline to be run.
 p.graph()
--- setup.py
+++ setup.py
@@ -9,49 +9,49 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file.
-with open(path.join(here, "README.rst")) as f:
+with open(path.join(here, 'README.rst')) as f:
     long_description = f.read()
 
 # Get package and author details.
 about = {}
-with open(path.join(here, "pypette", "__version__.py")) as f:
+with open(path.join(here, 'pypette', '__version__.py')) as f:
     exec(f.read(), about)
 
 setup(
     # Name of the module
-    name=about["__title__"],
+    name=about['__title__'],
     # Details
-    version=about["__version__"],
-    description=about["__description__"],
+    version=about['__version__'],
+    description=about['__description__'],
     long_description=long_description,
     # The project's main homepage.
-    url=about["__url__"],
+    url=about['__url__'],
     # Author details
-    author=about["__author__"],
-    author_email=about["__author_email__"],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
     # License
-    license=about["__license__"],
-    packages=[about["__title__"]],
-    test_suite="tests",
-    keywords="pipeline dev-tool threads multithreading python",
+    license=about['__license__'],
+    packages=[about['__title__']],
+    test_suite='tests',
+    keywords='pipeline dev-tool threads multithreading python',
     classifiers=[
         # Intended Audience.
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
         # License.
-        "License :: OSI Approved :: MIT License",
+        'License :: OSI Approved :: MIT License',
         # Project maturity.
-        "Development Status :: 3 - Alpha",
+        'Development Status :: 3 - Alpha',
         # Operating Systems.
-        "Operating System :: POSIX",
+        'Operating System :: POSIX',
         # Supported Languages.
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         # Topic tags.
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=["crayons"],
+    install_requires=['crayons'],
 )
